60f6e2c1f72ebe009705cfd4ee3f8179b960bea0
Adil <ajkkalappatt@gmail.com>
2022-11-23 15:11:22 +0530

    Added CO1 13th lab

---
+++ CO1/13.py
@@ -0,0 +1,4 @@
+list_fruits = ["apple", "banana", "cherry"]
+list_a_without_fruitss = [x for x in list_fruits if x != "apple"]
+print ("All list ",list_fruits)
+print("with out apple",list_a_without_fruitss)
